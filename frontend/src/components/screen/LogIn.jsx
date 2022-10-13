116bc441fb80cb21e04805498b5ed2b4bd19d144
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2022-10-13 22:26:57 +0530

    Added refs instead of states in forms

--- frontend/src/components/screen/LogIn.jsx
+++ frontend/src/components/screen/LogIn.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { useDispatch, useSelector } from 'react-redux';
 import { setTokens } from '../../redux/user/userSlice';
@@ -8,18 +8,18 @@ import Button from '../common/Button';
 const LogIn = () => {
   const dispatch = useDispatch();
   // TODO: remove default values
-  const [data, setData] = useState({
-    username: 'pinakipb2',
-    password: 'pinakipb2',
-  });
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
   const [logginIn, setLogginIn] = useState(false);
   const login = async () => {
+    const data = { username: usernameRef.current.value, password: passwordRef.current.value };
     console.log('Submitted', data);
-    //   // TODO: remove toast and setData
-    // setData({
-    //   username: '',
-    //   password: '',
-    // });
+    // TODO: remove toast
+    usernameRef.current.value = '';
+    passwordRef.current.value = '';
+    // TODO: Remove these 2 lines
+    usernameRef.current.value = 'pinakipb2';
+    passwordRef.current.value = 'pinakipb2';
     setLogginIn(true);
     const { status, message } = await signIn(data);
     if (status === 'success') {
@@ -30,27 +30,22 @@ const LogIn = () => {
     }
     setLogginIn(false);
   };
-  const handleChange = (e) => {
-    setData({
-      ...data,
-      [e.target.name]: e.target.value,
-    });
-  };
   return (
     <div className="h-full bg-blue-100 w-full">
       <div className="flex flex-col items-center justify-center w-full h-full gap-3">
         <div className="text-xl text-gray-600 mb-6 font-semibold">LogIn To Cuidly</div>
+        {/* TODO: Remove default value */}
         <div className="w-full justify-center items-center flex flex-col">
           <label htmlFor="username" className="mb-1 text-sm font-medium text-gray-500 w-1/6">
             Username
           </label>
-          <input type="text" id="username" name="username" className="p-1.5 px-2 rounded-md shadow-lg w-1/6" value={data.username} onChange={handleChange} />
+          <input type="text" id="username" className="p-1.5 px-2 rounded-md shadow-lg w-1/6" defaultValue="pinakipb2" ref={usernameRef} />
         </div>
         <div className="w-full justify-center items-center flex flex-col">
           <label htmlFor="password" className="mb-1 text-sm font-medium text-gray-500 w-1/6">
             Password
           </label>
-          <input type="password" id="password" name="password" className="p-1.5 px-2 rounded-md shadow-lg w-1/6" value={data.password} onChange={handleChange} />
+          <input type="password" id="password" className="p-1.5 px-2 rounded-md shadow-lg w-1/6" defaultValue="pinakipb2" ref={passwordRef} />
         </div>
         <Button buttonText="Log In" isDisabled={logginIn} isLoading={logginIn} loadingText="Logging In" onClick={login} />
       </div>
--- frontend/src/components/screen/SignUp.jsx
+++ frontend/src/components/screen/SignUp.jsx
@@ -1,63 +1,54 @@
-import React, { useState } from 'react';
-// import { registerUser } from '../api/auth';
+import React, { useRef, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { useRouter } from 'next/router';
-import { getSession, registerUserAuth } from '../../lib/auth';
-import Loading from '../screen/Loading';
+import { registerUserAuth } from '../../lib/auth';
 import Button from '../common/Button';
 
 const SignUp = () => {
-  const { data: session, status } = getSession();
-  console.table({ session, status });
   const router = useRouter();
   const [registering, setRegistering] = useState(false);
-  // TODO: remove dummy data
-  const [data, setData] = useState({
-    username: 'jackie',
-    password: 'jackie',
-  });
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
   const register = async () => {
-    console.log('Submitted', data);
     if (router.isReady) {
+      // TODO: remove toast
+      usernameRef.current.value = '';
+      passwordRef.current.value = '';
+      // TODO: Remove these 2 lines
+      usernameRef.current.value = 'jackal';
+      passwordRef.current.value = 'jackal';
       setRegistering(true);
-      const { status, message } = await registerUserAuth({ ...data, type: router.query.type || 'FREE' });
+      const data = {
+        username: usernameRef.current.value,
+        password: passwordRef.current.value,
+        type: router.query.type || 'FREE',
+      };
+      console.log('Submitted', data);
+      const { status, message } = await registerUserAuth(data);
       if (status === 'success') {
         toast.success('Account Created Successfully');
       } else {
         toast.error(message, { id: 'signup-error' });
       }
-      // setData({
-      //   username: '',
-      //   password: '',
-      // });
       setRegistering(false);
     }
   };
-  const handleChange = (e) => {
-    setData({
-      ...data,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  if (status === 'authenticated') {
-    return <Loading />;
-  }
   return (
     <div className="h-full bg-blue-100">
       <div className="flex flex-col items-center justify-center w-full h-full gap-3">
         <div className="text-xl text-gray-600 mb-6 font-semibold">Register An Account</div>
+        {/* TODO: Remove default values */}
         <div className="w-full justify-center items-center flex flex-col">
           <label htmlFor="username" className="mb-1 text-sm font-medium text-gray-500 w-1/6">
             Username
           </label>
-          <input type="text" id="username" name="username" className="p-1.5 px-2 rounded-md shadow-lg w-1/6" value={data.username} onChange={handleChange} />
+          <input type="text" id="username" className="p-1.5 px-2 rounded-md shadow-lg w-1/6" defaultValue="jackal" ref={usernameRef} />
         </div>
         <div className="w-full justify-center items-center flex flex-col">
           <label htmlFor="password" className="mb-1 text-sm font-medium text-gray-500 w-1/6">
             Password
           </label>
-          <input type="password" id="password" name="password" className="p-1.5 px-2 rounded-md shadow-lg w-1/6" value={data.password} onChange={handleChange} />
+          <input type="password" id="password" className="p-1.5 px-2 rounded-md shadow-lg w-1/6" defaultValue="jackal" ref={passwordRef} />
         </div>
         <Button buttonText="Register" isDisabled={registering} isLoading={registering} onClick={register} />
       </div>
